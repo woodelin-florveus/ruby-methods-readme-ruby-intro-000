041b046cee6af42ec6d444186e670438b442fe8c
Woodelin Florveus <woodelinflorveus@gmail.com>
2020-09-18 00:55:48 +0000

    Automatically backed up by Learn

--- lib/example.rb
+++ lib/example.rb
@@ -1,16 +1,16 @@
 # Use this space to code along with the readme
 
-phrase = "Hello World!"
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
+# phrase = "Hello World!"
+# puts phrase
+# puts phrase
+# puts phrase
+# puts phrase
+# puts phrase
+# puts phrase
+# puts phrase
+# puts phrase
+# puts phrase
+# puts phrase
 
 statement = "hello universe"
 
